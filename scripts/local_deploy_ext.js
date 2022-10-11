9d16ce51f0d6954d305b024b18c0973f85a3a82a
Daniel Erenshtein <daniel@bmybit.com>
2022-10-11 10:20:36 +0300

    Added ExtendedProtocolV3 and deployment scripts

--- scripts/local_deploy_ext.js
+++ scripts/local_deploy_ext.js
@@ -3,20 +3,20 @@ const { getTxReceipt } = require("../utils/getTxReceipt");
 
 async function main() {
     accounts = await ethers.getSigners();
-    // Deploy YandaToken
+    // // Deploy YandaToken
     const Token = await ethers.getContractFactory("YandaToken");
-    token = await upgrades.deployProxy(Token);
-    await token.deployed();
+    token = await Token.attach('0x3ed62137c5DB927cb137c26455969116BF0c23Cb');
+    // await token.deployed();
     // Deploy YandaProtocol
-    const Protocol = await ethers.getContractFactory("YandaExtendedProtocol");
-    protocol = await upgrades.deployProxy(Protocol, [10, 50, token.address]);
+    const Protocol = await ethers.getContractFactory("YandaExtendedProtocolV3");
+    protocol = await Protocol.deploy(10, 20736, token.address)
     await protocol.deployed();
 
-    console.log("YandaToken deployed at:", token.address);
+    // console.log("YandaToken deployed at:", token.address);
     console.log("YandaExtendedProtocol deployed at:", protocol.address);
 
     // Add new service with the admin address of accounts[1] and as validators accounts[2,3,4]
-    await protocol.addService(accounts[1].address, accounts[0].address, accounts.map(x => x.address).slice(2, 5));
+    await protocol.addService(accounts[1].address, accounts.map(x => x.address).slice(2, 5));
 
     // Transfer 10 YandaToken from token owner to the first validator(accounts[2])
     await token.transfer(accounts[2].address, ethers.utils.parseEther('10'));
