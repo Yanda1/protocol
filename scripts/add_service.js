97b4ee09d9d2f7daab799a4a14c5d442820e6f34
Daniel Erenshtein <daniel@bmybit.com>
2023-04-24 11:37:04 +0300

    chore: added staking into add_service script | de

--- scripts/add_service.js
+++ scripts/add_service.js
@@ -1,5 +1,6 @@
 const { ethers } = require("hardhat");
 require('dotenv').config({path: '.env'});
+const { getTxReceipt } = require("../utils/getTxReceipt");
 
 async function main() {
   if (!process.env.TOKEN_ADDR) {
@@ -30,6 +31,36 @@ async function main() {
   // Add new service with the admin address of accounts[1] and as validators accounts[3,4,5], fee nominator is 1 and fee denominator is 1
   console.log('Adding new service with args:', accounts[1].address, accounts.map(x => x.address).slice(3, 6));
   await protocol.addService(accounts[1].address, accounts.map(x => x.address).slice(3, 6));
+  
+  // Transfer 1000 YandaToken from token owner to the new validator(accounts[3])
+  await token.transfer(accounts[3].address, ethers.utils.parseEther('1000'));
+  // Transfer 2000 YandaToken from token owner to the new validator(accounts[4])
+  await token.transfer(accounts[4].address, ethers.utils.parseEther('2000'));
+  // Transfer 3000 YandaToken from token owner to the new validator(accounts[5])
+  await token.transfer(accounts[5].address, ethers.utils.parseEther('3000'));
+
+  // Stake to activate new validators
+  const stakeAmount1 = ethers.utils.parseEther('1000');
+  const tx1 = await token.connect(accounts[3]).approve(protocol.address, stakeAmount1);
+  // Wait until the transaction is mined
+  await getTxReceipt(tx1);
+  // Stake 1000 YandaToken from the new validator(accounts[3])
+  await protocol.connect(accounts[3]).stake(accounts[3].address, stakeAmount1);
+
+  const stakeAmount2 = ethers.utils.parseEther('2000');
+  const tx2 = await token.connect(accounts[4]).approve(protocol.address, stakeAmount2);
+  // Wait until the transaction is mined
+  await getTxReceipt(tx2);
+  // Stake 2000 YandaToken from the new validator(accounts[4])
+  await protocol.connect(accounts[4]).stake(accounts[4].address, stakeAmount2);
+
+  const stakeAmount3 = ethers.utils.parseEther('3000');
+  const tx3 = await token.connect(accounts[5]).approve(protocol.address, stakeAmount3);
+  // Wait until the transaction is mined
+  await getTxReceipt(tx3);
+  // Stake 3000 YandaToken from the new validator(accounts[5])
+  await protocol.connect(accounts[5]).stake(accounts[5].address, stakeAmount3);
+
   console.log('done!');
 }
 
